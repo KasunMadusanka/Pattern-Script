0311b95098e5fcd302adb2bb96f10c76fb72d889
Kasun Madusanka <kmadusanka@virtusapolaris.com>
2016-07-07 18:14:09 +0530

    support for DOM

--- patternScript.js
+++ patternScript.js
@@ -8,15 +8,15 @@ var ps = (function(){
     
     //// SINGLETON ////
     ///////////////////
-    var SingletonPattern = function(baseClass){
+    var SingletonPattern = function(baseClass) {
         if(baseClass == undefined)
             throw "EXCEPTION : base class requiered for creating a singleton object!\nPlease use 'new SingletonPattern(base_class)'";
-        var tempClass = (function(){
+        var tempClass = (function() {
             var instance;
             var Class = baseClass;
 
             return {
-                getInstance : function(){
+                getInstance : function() {
                     if(!instance){
                         instance = Class();
                     }
@@ -32,28 +32,42 @@ var ps = (function(){
     //////////////////
     var ObserverPattern = {
         Subject : function(baseClass){
-            if(baseClass == undefined)
-                throw "EXCEPTION : base class requiered for creating a Subject class!\nPlease use 'new ObserverPattern.Subject(base_class)'";
-            baseClass.prototype.observers = [];
-            baseClass.prototype.addObserver = function(observer){
+            var addObserver = function(observer) {
                 this.observers.push(observer);
             };
-            baseClass.prototype.removeObserver = function(observer){
+            var removeObserver = function(observer) {
                 this.observers.splice(this.observers.indexOf(observer), 1);
             };
-            baseClass.prototype.notify = function(notification){
+            var notify = function(notification) {
                 for(var index in this.observers){
                     this.observers[index].update(notification);
                 }
             };
+            if(baseClass == undefined)
+                throw "EXCEPTION : base class requiered for creating a Subject class!\nPlease use 'new ObserverPattern.Subject(base_class)'";
+            if(!baseClass.prototype) {
+                baseClass.observers = [];
+                baseClass.addObserver = addObserver;
+                baseClass.removeObserver = removeObserver;
+                baseClass.notify = notify;
+            } else {
+                baseClass.prototype.observers = [];
+                baseClass.prototype.addObserver = addObserver;
+                baseClass.prototype.removeObserver = removeObserver;
+                baseClass.prototype.notify = notify;
+            }
             return baseClass;
         },
-        Observer : function(baseClass, updateFunction){
+        Observer : function(baseClass, updateFunction) {
             if(baseClass == undefined)
                 throw "EXCEPTION : base class requiered for creating a Observer class!\nPlease use 'new ObserverPattern.Observer(base_class)'";
             if(updateFunction == undefined)
                 throw "EXCEPTION : function for update is compulsory!\nPlease use 'new ObserverPattern.Observer(baseClass, update_function)'";
-            baseClass.prototype.update = updateFunction;
+            if(!baseClass.prototype) {
+                baseClass.update = updateFunction;
+            } else {
+                baseClass.prototype.update = updateFunction;
+            }
             return baseClass;
         }
     }
@@ -62,11 +76,8 @@ var ps = (function(){
     //// PUBLISHER-SUBSCRIBER ////
     //////////////////////////////
     var PublisherSubscriberPattern = {
-        Publisher : function(baseClass){
-            if(baseClass == undefined)
-                throw "EXCEPTION : base class requiered for creating a Publisher class!\nPlease use 'new PublisherSubscriberPattern.Publisher(base_class)'";
-            baseClass.prototype.subscriptions = {};
-            baseClass.prototype.subscribe = function(newSubscriber, key = 'default'){
+        Publisher : function(baseClass) {
+            var subscribe = function(newSubscriber, key = 'default') {
                 if(newSubscriber == undefined)
                     throw "EXCEPTION : subscriber object required for make a subscription!\nPlease use 'PublisherSubscriberPattern.Subscriber' to create a new subscriber";
                 if (key in this.subscriptions){
@@ -75,27 +86,44 @@ var ps = (function(){
                     this.subscriptions[key] = [newSubscriber];
                 }
             };
-            baseClass.prototype.unsubscribe = function(subscriber, key = 'default'){
+            var unsubscribe = function(subscriber, key = 'default') {
                 this.subscriptions[key].splice(this.subscriptions[key].indexOf(subscriber), 1);
             };
-            baseClass.prototype.publish = function(context, key = 'default'){
+            var publish = function(context, key = 'default') {
                 for(k in this.subscriptions){
                     if(k == key){
-                        for(sub in this.subscriptions[key]){
+                        for(sub in this.subscriptions[key]) {
                             this.subscriptions[key][sub].listen(context);
                         }
                     }
                 }
             };
+            if(baseClass == undefined)
+                throw "EXCEPTION : base class requiered for creating a Publisher class!\nPlease use 'new PublisherSubscriberPattern.Publisher(base_class)'";
+            if(!baseClass.prototype) {
+                baseClass.subscriptions = {};
+                baseClass.subscribe = subscribe;
+                baseClass.unsubscribe = unsubscribe;
+                baseClass.publish = publish;
+            } else {
+                baseClass.prototype.subscriptions = {};
+                baseClass.prototype.subscribe = subscribe;
+                baseClass.prototype.unsubscribe = unsubscribe;
+                baseClass.prototype.publish = publish;
+            }
             return baseClass;
         },
         
-        Subscriber : function(baseClass, listenFunction){
+        Subscriber : function(baseClass, listenFunction) {
             if(baseClass == undefined)
                 throw "EXCEPTION : base class requiered for creating a Observer class!\nPlease use 'new ObserverPattern.Observer(base_class)'";
             if(listenFunction == undefined)
                 throw "EXCEPTION : function for update is compulsory!\nPlease use 'new ObserverPattern.Observer(baseClass, update_function)'";
-            baseClass.prototype.listen = listenFunction;
+            if(!baseClass.prototype) {
+                baseClass.listen = listenFunction;
+            } else {
+                baseClass.prototype.listen = listenFunction;
+            }
             return baseClass;
         }
     }
